e2d00d7bfb95d009e69181a975448b317f5e245e
221801333lsl <1390476733@qq.com>
2020-02-15 03:54:25 +0800

    笔记添加定位信息，在noteview,newactivity,resiveactivity,savedactivity的布局文件中添加展示定位信息的TextView。所有关于表的创建、添加数据和columns都添加location。Noteitem类、NoteAdapter添加location。数据传输除查询和提醒功能外bundle都添加location。应用名称suishouji改为随手记。此次commit于2月14号。

--- 随手记/app/src/main/java/com/example/suishouji/Noteitem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.suishouji
-
-import java.io.Serializable
-
-class Noteitem : Serializable{
-    var time:String?=null
-    var title:String?=null
-    var content:String?=null
-}
