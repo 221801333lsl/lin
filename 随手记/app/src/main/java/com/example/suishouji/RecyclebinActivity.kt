ffd045a1014895c977c22de230bef209081f5e99
221801333lsl <1390476733@qq.com>
2020-02-15 03:17:35 +0800

    RecyclebinActivity适配器DelectAdapter添加单击事件，被点击的item的ischecked改变后为true时将其position添加到数组，反正移除。点击移除则将数组中所有position对应的数据从表rb中删除后刷新Recyclebin界面，点击还原则将数据添加到表ssj中，并从表rb中删除后刷新Recyclebin界面。此次commit于2月13号。

--- 随手记/app/src/main/java/com/example/suishouji/RecyclebinActivity.kt
+++ 随手记/app/src/main/java/com/example/suishouji/RecyclebinActivity.kt
@@ -48,6 +48,14 @@ class RecyclebinActivity: AppCompatActivity(){
         btnSelectAll =  findViewById(com.example.suishouji.R.id.btn_select_all)
         btnBack = findViewById(com.example.suishouji.R.id.btn_back)
 
+        btnDel.setOnClickListener{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                delSelections()
+            }
+        }
+        btnBack.setOnClickListener{
+            backSelections()
+        }
         btnSelectAll.setOnClickListener{
             var alertDialog: AlertDialog = AlertDialog.Builder(this).create()
             alertDialog.setMessage("您需要的操作是：")
@@ -141,6 +149,88 @@ class RecyclebinActivity: AppCompatActivity(){
             }
         }
         adapter=DelectAdapter(this,lists)
+        adapter.setOnItemClickListener(object:DelectAdapter.OnItemClickListener{
+            override fun onItemClick(view: View, position: Int):Boolean {
+                if(checkList?.contains(position.toString())!!){
+                    checkList = checkList?.minus(position.toString())
+                }
+                else{
+                    checkList = checkList?.plus(position.toString())
+                }
+                return true
+            }
+        })
         hszrecycleview.setAdapter(adapter)
+    }
+    //删除
+    @SuppressLint("NewApi")
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun delSelections() {
+        var alertDialog: AlertDialog = AlertDialog.Builder(this).create()
+        alertDialog.setMessage("您需要的操作是：")
+        if (checkList ==null) {
+            alertDialog.setMessage("当前未选中项目")
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE,"确认",
+                DialogInterface.OnClickListener { dialog, which ->
+                })
+        } else {
+            alertDialog.setMessage("确认移除所选项目？")
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,"移除",
+                DialogInterface.OnClickListener { dialog, which ->
+                    for (i in checkList!!.indices) {
+                        if(adapter.Notebeans[i].title !=null){
+                            aweq=adapter.Notebeans[i].title.toString()
+                        }
+                        var delectarray:Array<String>?
+                        delectarray= arrayOf(aweq)
+                        database.use { delete("rb", "title" + " = ?",delectarray) }
+                        Toast.makeText(this,"移除成功", Toast.LENGTH_SHORT).show()
+                    }
+                    onResume()
+                })
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE,"取消",
+                DialogInterface.OnClickListener { dialog, which ->
+                })
+        }
+        alertDialog.show()
+    }
+
+    private fun backSelections() {
+        var alertDialog: AlertDialog = AlertDialog.Builder(this).create()
+        alertDialog.setMessage("您需要的操作是：")
+        if (checkList ==null) {
+            alertDialog.setMessage("当前未选中项目")
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE,"确认",
+                DialogInterface.OnClickListener { dialog, which ->
+                })
+        } else {
+            alertDialog.setMessage("确认还原所选项目？")
+            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,"还原",
+                DialogInterface.OnClickListener { dialog, which ->
+                    for (i in checkList!!.indices) {
+                        database.use {
+                            insert(
+                                "ssj",
+                                "title" to adapter.Notebeans[i].title.toString(),
+                                "time" to adapter.Notebeans[i].time.toString(),
+                                "content" to adapter.Notebeans[i].content.toString()
+                            )
+                        }
+                        if(adapter.Notebeans[i].title !=null){
+                            aweq=adapter.Notebeans[i].title.toString()
+                        }
+                        var delectarray:Array<String>?
+                        delectarray= arrayOf(aweq)
+                        database.use { delete("rb", "title" + " = ?",delectarray) }
+                        Toast.makeText(this,"还原成功", Toast.LENGTH_SHORT).show()
+                        onResume()
+                    }
+                })
+            alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE,"取消",
+                DialogInterface.OnClickListener { dialog, which ->
+                })
+        }
+        alertDialog.show()
     }
 }
